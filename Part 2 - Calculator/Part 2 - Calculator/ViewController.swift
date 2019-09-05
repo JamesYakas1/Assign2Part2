70bd157b291d49ec1873c9a2f11a47f4e940948d
James Yakas <YAKASJ02@myunitec.ac.nz>
2019-09-05 18:34:54 +1200

    Created project
    Created the intial project files.

---
+++ Part 2 - Calculator/Part 2 - Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Part 2 - Calculator
+//
+//  Created by User on 5/9/19.
+//  Copyright © 2019 Unitec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
