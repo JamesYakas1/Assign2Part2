07d2d7a5165f0ccf433cb3c69c1eb631a52702c0
James Yakas <YAKASJ02@myunitec.ac.nz>
2019-09-06 01:38:34 +1200

    Added results reference and button events
    Started phase 1 of the backend.
    
    Created reference to the results label.
    Created the event functions for the buttons.
    
    Set the AC button to reset the display.
    Tested the equals button for testing functionality.

--- Part 2 - Calculator/Part 2 - Calculator/ViewController.swift
+++ Part 2 - Calculator/Part 2 - Calculator/ViewController.swift
@@ -9,12 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //References
+    @IBOutlet weak var displayResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    //Numerical characters
+    @IBAction func selectDot(_ sender: Any) {
+    }
+    @IBAction func select0(_ sender: Any) {
+    }
+    @IBAction func select1(_ sender: Any) {
+    }
+    @IBAction func select2(_ sender: Any) {
+    }
+    @IBAction func select3(_ sender: Any) {
+    }
+    @IBAction func select4(_ sender: Any) {
+    }
+    @IBAction func select5(_ sender: Any) {
+    }
+    @IBAction func select6(_ sender: Any) {
+    }
+    @IBAction func select7(_ sender: Any) {
+    }
+    
+    @IBAction func select8(_ sender: Any) {
+    }
+    @IBAction func select9(_ sender: Any) {
+    }
+    
+    //Divide, multiply, minus and plus
+    
+    @IBAction func selectPlus(_ sender: Any) {
+    }
+    
+    @IBAction func selectMinus(_ sender: Any) {
+    }
+    
+    @IBAction func selectMultiply(_ sender: Any) {
+    }
+    
+    @IBAction func selectdDivide(_ sender: Any) {
+    }
+    
+    //Equals
+    @IBAction func selectEquals(_ sender: Any) {
+        displayResult.text = "Equals tapped"
+    }
+    
+    //Clear Entry
+    @IBAction func selectCE(_ sender: Any) {
+    }
+    
+    //All clear
+    @IBAction func selectAC(_ sender: Any) {
+        displayResult.text = "";
+    }
+    
+    
 }
 
